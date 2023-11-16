25f12ef220c1c60d1eaa4f21eecf4e8db8b7bbad
GrahamZen <931252924@qq.com>
2023-11-15 22:49:39 -0500

    add skybox add postprocess mat

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -174,3 +174,165 @@ void Stepify_float(float val, float min_val, float max_val, float step, out floa
     float step_val = floor((val - min_val) / step_size);
     out_val = min_val + step_val * step_size;
 }
+
+float3 convertAdjustColorLevel(float3 colorLevel) {
+    float scale = 0.01;
+    float redAdjustment = -colorLevel.x * scale; 
+    float greenAdjustment = -colorLevel.y * scale; 
+    float blueAdjustment = colorLevel.z * scale; 
+
+    return float3(redAdjustment, greenAdjustment, blueAdjustment);
+}
+
+float3 AdjustColorBalance(float3 color, float3 shadowsAdjustments, float3 midtonesAdjustments, float3 highlightsAdjustments)
+{
+    float luminance = dot(color, float3(0.2126, 0.7152, 0.0722));
+    float3 shadows = saturate((0.5 - luminance) * 2.0);
+    float3 highlights = saturate((luminance - 0.5) * 2.0);
+    float3 midtones = 1.0 - shadows - highlights;
+
+    color += shadowsAdjustments * shadows;
+    color += midtonesAdjustments * midtones;
+    color += highlightsAdjustments * highlights;
+
+    return saturate(color);
+}
+
+#define PI 3.14159265359
+#define EPS 0.00001
+
+float fPow5(float v)
+{
+    return pow(1 - v, 5);
+}
+
+// Diffuse distribution functions
+
+float3 lambertDiffuse(float3 albedo)
+{
+    return albedo / PI;
+}
+
+// Fresnel functions
+
+float3 fresnel(float3 F0, float NdotV)
+{
+    return F0 + (1 - F0) * fPow5(NdotV);
+}
+
+float3 fresnel(float3 F0, float NdotV, float roughness)
+{
+    return F0 + (max(1.0 - roughness, F0) - F0) * fPow5(NdotV);
+}
+
+float3 fresnelDisney(float HdotL, float NdotL, float NdotV, float roughness)
+{
+    float k = 0.5 + 2 * roughness * sqrt(HdotL);
+    float firstTerm = (k - 1) * fPow5(NdotV) + 1;
+    float secondTerm = (k - 1) * fPow5(NdotL) + 1;
+    return firstTerm * secondTerm;
+}
+
+float3 F0(float ior)
+{
+    return pow((1.0 - ior) / (1.0 + ior), 2);
+}
+
+// Normal distribution functions
+
+float trowbridgeReitzNDF(float NdotH, float roughness)
+{
+    float alpha = roughness * roughness;
+    float alpha2 = alpha * alpha;
+    float NdotH2 = NdotH * NdotH;
+    float denominator = PI * pow((alpha2 - 1) * NdotH2 + 1, 2);
+    return alpha2 / denominator;
+}
+
+float trowbridgeReitzAnisotropicNDF(float NdotH, float roughness, float anisotropy, float HdotT, float HdotB)
+{
+    float aspect = sqrt(1.0 - 0.9 * anisotropy);
+    float alpha = roughness * roughness;
+
+    float roughT = alpha / aspect;
+    float roughB = alpha * aspect;
+
+    float alpha2 = alpha * alpha;
+    float NdotH2 = NdotH * NdotH;
+    float HdotT2 = HdotT * HdotT;
+    float HdotB2 = HdotB * HdotB;
+
+    float denominator = PI * roughT * roughB * pow(HdotT2 / (roughT * roughT) + HdotB2 / (roughB * roughB) + NdotH2, 2);
+    return 1 / denominator;
+}
+
+// Geometric attenuation functions
+
+float cookTorranceGAF(float NdotH, float NdotV, float HdotV, float NdotL)
+{
+    float firstTerm = 2 * NdotH * NdotV / HdotV;
+    float secondTerm = 2 * NdotH * NdotL / HdotV;
+    return min(1, min(firstTerm, secondTerm));
+}
+
+float schlickBeckmannGAF(float dotProduct, float roughness)
+{
+    float alpha = roughness * roughness;
+    float k = alpha * 0.797884560803;  // sqrt(2 / PI)
+    return dotProduct / (dotProduct * (1 - k) + k);
+}
+
+// Helpers
+float3 gammaCorrection(float3 v)
+{
+    return pow(v, 1.0 / 2.2);
+}
+
+float3 sRGB2Lin(float3 col)
+{
+    return pow(col, 2.2);
+}
+
+float3 CalculatePBRColor(float3 normal, float3 viewVec, float3 lightVec, float3 albedo, float roughness, float metalness)
+{
+    float3 halfVec = normalize(lightVec + viewVec);
+    float NdotL = max(dot(normal, lightVec), 0.0);
+    float NdotH = max(dot(normal, halfVec), 0.0);
+    float NdotV = max(dot(normal, viewVec), 0.0);
+
+    float3 F0 = lerp(float3(0.04, 0.04, 0.04), albedo, metalness);
+    float D = trowbridgeReitzNDF(NdotH, roughness);
+    float3 F = fresnel(F0, NdotV, roughness);
+    float G = schlickBeckmannGAF(NdotV, roughness) * schlickBeckmannGAF(NdotL, roughness);
+
+    float3 brdfOutput = (D * F * G) / (4 * NdotV * NdotL + 0.001);
+
+    return brdfOutput;
+}
+
+float3 PBR(float3 worldPos, float3 normal, float3 viewVec, float3 albedo, float roughness, float metalness)
+{
+    float3 mainLightColor;
+    float3 mainLightDir;
+    float mainLightDistanceAtten;
+    float mainLightShadowAtten;
+
+    GetMainLight_float(worldPos, mainLightColor, mainLightDir, mainLightDistanceAtten, mainLightShadowAtten);
+    float3 brdfOutput = CalculatePBRColor(normal, viewVec, mainLightDir, albedo * 100.f, roughness, metalness);
+    return brdfOutput * mainLightColor;
+}
+
+void Postprocess_float(float3 worldPos, float3 normal, float3 viewVec, float3 albedo, float roughness, float metalness,
+    float3 Shadows, float3 Midtones, float3 Highlights,
+    out float3 OUT)
+{
+    float3 mainLightColor;
+    float3 mainLightDir;
+    float mainLightDistanceAtten;
+    float mainLightShadowAtten;
+
+    GetMainLight_float(worldPos, mainLightColor, mainLightDir, mainLightDistanceAtten, mainLightShadowAtten);
+    const float3 col = BlinnPhongOneLight(normal, viewVec, worldPos, mainLightColor, mainLightDir, mainLightDistanceAtten, mainLightShadowAtten, 0, 0, 0);
+    // OUT = AdjustColorBalance(pbrCol, convertAdjustColorLevel(Shadows), convertAdjustColorLevel(Midtones), convertAdjustColorLevel(Highlights));
+    OUT = AdjustColorBalance(saturate(col), convertAdjustColorLevel(Shadows), convertAdjustColorLevel(Midtones), convertAdjustColorLevel(Highlights));
+}
