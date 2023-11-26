039e2f79cf1a7b42f920139efac28d5ebb536305
Tongwei Dai <tongweid@gmail.com>
2023-11-26 00:53:45 -0500

    ground update

--- Assets/Shaders/Includes/Ground.hlsl
+++ Assets/Shaders/Includes/Ground.hlsl
@@ -5,6 +5,41 @@ void shade_float(float2 uv, float2 sz,  float bloom, float3 dark, float3 bright,
     col = lerp(dark, bright, clamp(lines.x + lines.y, 0.0, 3.0));
 }
 
+inline float rd (float2 uv) {
+    return frac(sin(dot(uv, float2(12.9898, 78.233)))*43758.5453);
+}
+inline float noise (float2 uv) {
+    float2 i = floor(uv);
+    float2 f = frac(uv);
+    f = f * f * (3.0 - 2.0 * f);
+
+    uv = abs(frac(uv) - 0.5);
+    float2 c0 = i + float2(0.0, 0.0);
+    float2 c1 = i + float2(1.0, 0.0);
+    float2 c2 = i + float2(0.0, 1.0);
+    float2 c3 = i + float2(1.0, 1.0);
+    float r0 = rd(c0);
+    float r1 = rd(c1);
+    float r2 = rd(c2);
+    float r3 = rd(c3);
+    float t = lerp(lerp(r0, r1, f.x), lerp(r2, r3, f.x), f.y);
+    return t;
+}
+float noise(float2 uv, float scale) {
+    float t = 0.0;
+    float freq = pow(2.0, float(0));
+    float amp = pow(0.5, float(3));
+    t += noise(float2(uv.x*scale/freq, uv.y*scale/freq))*amp;
+
+    freq = pow(2.0, float(1));
+    amp = pow(0.5, float(3-1));
+    t += noise(float2(uv.x*scale/freq, uv.y*scale/freq))*amp;
+
+    freq = pow(2.0, float(2));
+    amp = pow(0.5, float(3-2));
+    t += noise(float2(uv.x*scale/freq, uv.y*scale/freq))*amp;
+    return t;
+}
 // From https://www.shadertoy.com/view/WttXWX
 uint hash(uint x) {
     x ^= x >> 16;
@@ -14,22 +49,21 @@ uint hash(uint x) {
     x ^= x >> 16;
     return x;
 }
-
 float hash(float2 vf) {
     uint offset = vf.x < 0.0 ? 13u : 0u;
     uint2 vi = uint2(abs(vf));
-    return float(hash(vi.x + (vi.y<<16) + offset)) / float(0xffffffffU);
+    return float(hash(vi.x + (vi.y<<16) + offset)) / float( 0xffffffffU );
 }
-
-void vertex_height_float(float3 pos, float hi, float xthres, float xmax, out float3 outpos) {
+void vertex_height_float(float3 pos, float hi, float speed, float xthres, float xmax, out float3 outpos) {
+    float2 sp = float2(pos.x, pos.z - speed * _Time.x);
     float offset = 0.0;
     if (abs(pos.x) >= xthres) {
         // smooth step between 0 and hi
         float t = (abs(pos.x) - xthres) / (xmax - xthres); // [0,1]
         t *= hi; // [0,hi]
-        pos.y += smoothstep(0, hi, t) + hash(pos.xz * 4.0) * 0.4;
+        pos.y += smoothstep(0, hi, t) + noise(sp * 2.0, 2.2);
     } else {
-        pos.y += hash(pos.xz * 4.0) * 0.1;
+        pos.y += noise(sp * 2.0) * 0.1;
     }
     
     outpos = pos;
